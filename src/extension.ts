e0b28c6233eb5bd6bc39bfda98637b551a3a6baa
Alexandr <l4legenda@gmail.com>
2023-03-22 22:38:06 +0500

    feat: deep in vscode extension

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,17 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { generateApolloClient } from '@deep-foundation/hasura/client';
+import { DeepClient } from '@deep-foundation/deeplinks/imports/client';
+
+const rootClient = generateApolloClient({
+	path: '3006-deepfoundation-dev-9xrko488biv.ws-eu92.gitpod.io/gql',
+	ssl: true,
+});
+
+const unloginedDeep = new DeepClient({
+	apolloClient: rootClient,
+});
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
@@ -13,9 +24,13 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('vscode-deep-extension.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
+	let disposable = vscode.commands.registerCommand('vscode-deep-extension.helloWorld', async () => {
+
+		const guest = await unloginedDeep.guest();
+		const guestDeep = new DeepClient({ deep: unloginedDeep, ...guest });
+		const admin = await guestDeep.login({ linkId: await guestDeep.id('deep', 'admin') });
+		const deep = new DeepClient({ deep: guestDeep, ...admin });
+
 		vscode.window.showInformationMessage('Hello World from vscode-deep-extension!');
 	});
 
@@ -23,4 +38,4 @@ export function activate(context: vscode.ExtensionContext) {
 }
 
 // This method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() { }
